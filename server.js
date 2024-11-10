21f14cb9dbe8ace00543129b19544e082ee87481
Divya Rathod <drtas2521@gmail.com>
2024-11-10 13:49:11 +0530

    DB connection added

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const cors = require('cors');
 const morgan  = require('morgan');
 const dotenv = require('dotenv');
 const colors = require('colors');
+const connectDB = require('./config/connectDB');
 
 //config env file 
 dotenv.config();
+
+//database call
+connectDB();
 
 //rest object 
 const app = express();
@@ -26,5 +30,5 @@ const PORT = 8080 || process.env.PORT;
 
 //listen
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port ${PORT}`.blue)
 });
